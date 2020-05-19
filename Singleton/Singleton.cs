981c973adc4b8a129c0947a3fc7bc35f878923a1
Tin August Levačić <tilevaci@vsite.hr>
2020-05-19 12:12:47 +0200

    Created Singleton class, created basic UI

--- Singleton/Singleton.cs
+++ Singleton/Singleton.cs
@@ -13,6 +13,7 @@ namespace Singleton
         private static Singleton instance;
         private Singleton() { }
 
+
         public static Singleton getInstance()
         {
             if (instance == null)
