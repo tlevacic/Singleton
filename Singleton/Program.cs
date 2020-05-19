79a63ebe3850a5d473e1b8d6eea5bb59abcdfb42
Tin August Levačić <tilevaci@vsite.hr>
2020-05-19 13:13:32 +0200

    Added another form, which will open login form

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -16,7 +16,7 @@ namespace Singleton
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new WelcomeForm());
         }
     }
 }
---
+++ Singleton/WelcomeForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Singleton
+{
+    partial class WelcomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(262, 106);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(213, 55);
+            this.login.TabIndex = 0;
+            this.login.Text = "LogIn";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.openLoginForm);
+            // 
+            // WelcomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.login);
+            this.Name = "WelcomeForm";
+            this.Text = "WelcomeForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button login;
+    }
+}
