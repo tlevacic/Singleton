628565dd617efcfc6f051d33fb7eb16c01bf2e61
Tin August Levačić <tilevaci@vsite.hr>
2020-05-20 19:19:26 +0200

    Added border radius on form for login

--- Singleton/LoginForm.Designer.cs
+++ Singleton/LoginForm.Designer.cs
@@ -35,11 +35,13 @@
             this.login = new System.Windows.Forms.Button();
             this.cancel = new System.Windows.Forms.Button();
             this.errorInfo = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // username
             // 
-            this.username.Location = new System.Drawing.Point(178, 48);
+            this.username.Location = new System.Drawing.Point(33, 119);
             this.username.Name = "username";
             this.username.Size = new System.Drawing.Size(364, 20);
             this.username.TabIndex = 0;
@@ -51,14 +53,14 @@
             // 
             // password
             // 
-            this.password.Location = new System.Drawing.Point(178, 95);
+            this.password.Location = new System.Drawing.Point(744, 200);
             this.password.Name = "password";
             this.password.Size = new System.Drawing.Size(364, 20);
             this.password.TabIndex = 2;
             // 
             // login
             // 
-            this.login.Location = new System.Drawing.Point(178, 199);
+            this.login.Location = new System.Drawing.Point(15, 438);
             this.login.Name = "login";
             this.login.Size = new System.Drawing.Size(75, 23);
             this.login.TabIndex = 3;
@@ -68,7 +70,7 @@
             // 
             // cancel
             // 
-            this.cancel.Location = new System.Drawing.Point(378, 199);
+            this.cancel.Location = new System.Drawing.Point(819, 378);
             this.cancel.Name = "cancel";
             this.cancel.Size = new System.Drawing.Size(75, 23);
             this.cancel.TabIndex = 4;
@@ -78,25 +80,38 @@
             // errorInfo
             // 
             this.errorInfo.AutoSize = true;
-            this.errorInfo.Location = new System.Drawing.Point(178, 140);
+            this.errorInfo.Location = new System.Drawing.Point(12, 388);
             this.errorInfo.Name = "errorInfo";
             this.errorInfo.Size = new System.Drawing.Size(35, 13);
             this.errorInfo.TabIndex = 5;
             this.errorInfo.Text = "label1";
             this.errorInfo.Visible = false;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.username);
+            this.panel1.Location = new System.Drawing.Point(313, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(397, 471);
+            this.panel1.TabIndex = 6;
+            // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1065, 510);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.errorInfo);
             this.Controls.Add(this.cancel);
             this.Controls.Add(this.login);
             this.Controls.Add(this.password);
-            this.Controls.Add(this.username);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "LoginForm";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,6 +125,7 @@
         private System.Windows.Forms.Button login;
         private System.Windows.Forms.Button cancel;
         private System.Windows.Forms.Label errorInfo;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Singleton/LoginForm.cs
+++ Singleton/LoginForm.cs
@@ -7,16 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
+
 
 namespace Singleton
 {
     public partial class LoginForm : Form
     {
         Singleton instanceOfSingleton;
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+        private void set_background(Object sender, PaintEventArgs e)
+        {
+            var size = this.Size;
+            Graphics graphics = e.Graphics;
+            Rectangle gradient_rectangle = new Rectangle(0, 0, size.Width, size.Height);
+            Brush b = new LinearGradientBrush(gradient_rectangle, Color.FromArgb(64, 224, 208), Color.FromArgb(238, 130, 238), 59f);      
+            graphics.FillRectangle(b, gradient_rectangle);
+        }
+
         public LoginForm()
         {
             InitializeComponent();
+
+
+            //Background
+            this.Paint += new PaintEventHandler(set_background);
             instanceOfSingleton = Singleton.getInstance();
+
+            //Border radius
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+
             username.Text = instanceOfSingleton.Username;
             password.Text = instanceOfSingleton.Password;
         }
